da10fd49b4000eb1c6171e0b1f2577b897095e62
AdhamUsama <adhamusama25@gmail.com>
2024-04-01 22:37:40 +0200

    Set up MUI Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import CustomModal from "./components/UI/CustomModal/CustomModal";
+import { Button, Container, Grid } from "@mui/material";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -8,13 +9,32 @@ function App() {
 
   return (
     <>
-      <CustomModal isModalOpen={isModalOpen} closeModal={closeModal}>
-        <h1>Modal Content</h1>
-        <p>This is a modal content</p>
-      </CustomModal>
-      <p>Test</p>
-      <button onClick={openModal}>Open Modal</button>
-      <button onClick={closeModal}>Close Modal</button>
+      <Container
+        style={{
+          display: "flex",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CustomModal isModalOpen={isModalOpen} closeModal={closeModal}>
+          <h1>Modal Content</h1>
+          <p>This is a modal content</p>
+        </CustomModal>
+
+        <Grid container spacing={1} style={{ width: "25%" }}>
+          <Grid item xs={6}>
+            <Button variant="contained" onClick={openModal}>
+              Open Modal
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="outlined" onClick={closeModal}>
+              Close Modal
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
