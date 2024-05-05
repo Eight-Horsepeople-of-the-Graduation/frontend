c04b755d39d5ad1aa90224ee1752f4292fb9bbae
AdhamUsama <adhamusama25@gmail.com>
2024-05-05 03:18:15 +0300

    Optionally hide side pannel if hideOnScrollTo value is gavin to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import SidePannel from "../SidePannel/SidePannel";
 import classes from "./Layout.module.css";
 import Header from "../Header/Header";
 
 interface LayoutProps {
   children: React.ReactNode;
+  hideOnScrollTo?: number;
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, hideOnScrollTo }: LayoutProps) => {
+  const [hidePannel, setHidePannel] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  contentRef.current?.addEventListener("scroll", () => {
+    if (!contentRef.current) return;
+
+    if (hideOnScrollTo && contentRef.current.scrollTop > hideOnScrollTo) {
+      setHidePannel(true);
+    } else {
+      setHidePannel(false);
+    }
+  });
+
   return (
     <>
       <Header />
       <div className={classes.layout}>
-        <div className={classes.content}>{children}</div>
-        <div className={classes.sidePannelContainer}>
+        <div className={classes.content} ref={contentRef}>
+          {children}
+        </div>
+        <div
+          className={[
+            classes.sidePannelContainer,
+            hidePannel && classes.Hiden,
+          ].join(" ")}
+        >
           <SidePannel />
         </div>
       </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,11 @@ import Layout from "../../components/Layout/Layout";
 const HomePage = () => {
   document.title = "Readify | Home";
 
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <h1>Home Page</h1>
+    </Layout>
+  );
 };
 
 export default HomePage;
