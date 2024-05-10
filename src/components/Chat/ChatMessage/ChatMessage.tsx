67cee619eaa507345a7dcc8243237c1751ac071d
AdhamUsama <adhamusama25@gmail.com>
2024-05-10 08:14:53 +0300

    move ai user to separate file

--- src/components/Chat/ChatMessage/ChatMessage.tsx
+++ src/components/Chat/ChatMessage/ChatMessage.tsx
@@ -1,29 +1,19 @@
-import { User } from "../../../Types/users.types";
 import { useAppSelector } from "../../../redux/hooks";
 import CustomAvatar from "../../UI/CustomAvatar/CustomAvatar";
+import AiUser from "../AiUser";
 import classes from "./ChatMessage.module.css";
-import logo from "../../../assets/images/logo.png";
 
 interface ChatMessageProps {
   message: string;
   fromAi: boolean;
 }
 
-export const Ai: User = {
-  email: "ai@readify.com",
-  id: 0,
-  image: logo,
-  name: "A I",
-  role: "Ai",
-  username: "Ai",
-};
-
 const ChatMessage = ({ message, fromAi }: ChatMessageProps) => {
   const user = useAppSelector((state) => state.authUser.user);
   return (
     <div className={classes.ChatMessage}>
       <CustomAvatar
-        user={fromAi ? Ai : user!}
+        user={fromAi ? AiUser : user!}
         size="m"
         style={{ boxShadow: "var(--shadow-far)" }}
       />
