df6abc2dc0efd5c8677d1ee8e310ab2dee63b2b4
ranaagouda <ranagouda01@gmail.com>
2024-05-11 14:24:57 +0300

    added ImageCarousel to HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import SidePannelLayout from "../../components/SidePannelLayout/SidePannelLayout";
 import ListPreview from "../../components/ListPreview/ListPreview";
 import { dummyLists } from "../../dummyData";
+import ImageCarousel from "../../components/ImageCarousel/ImageCarosel";
 
 const HomePage = () => {
   document.title = "Readify | Home";
@@ -8,6 +9,7 @@ const HomePage = () => {
   return (
     <SidePannelLayout>
       <main style={{ width: "100%", margin: "48px 48px" }}>
+        <ImageCarousel url={""}/>
         {dummyLists
           .filter((list) => list.books.length)
           .map((list) => (
