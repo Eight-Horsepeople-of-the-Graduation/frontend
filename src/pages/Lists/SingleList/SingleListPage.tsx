56445b098634e5b16e5db1c5e3e8b182a0325cbe
AdhamUsama <adhamusama25@gmail.com>
2024-05-11 16:15:07 +0300

    edit name and privacy and remove book from list

---
+++ src/pages/Lists/SingleList/SingleListPage.tsx
@@ -0,0 +1,161 @@
+import { useParams } from "react-router-dom";
+import SidePannelLayout from "../../../components/SidePannelLayout/SidePannelLayout";
+import { dummyLists } from "../../../dummyData";
+import BookComponent from "../../../components/BookComponent/BookComponent";
+import PageNotFoundPage from "../../PageNotFound/PageNotFoundPage";
+import classes from "./SingleListPage.module.css";
+import EditIcon from "@mui/icons-material/Edit";
+import PrivacySwitch from "../../../components/PrivacySwitch/PrivacySwitch";
+import { Button } from "@mui/material";
+import { useRef, useState } from "react";
+import CheckIcon from "@mui/icons-material/Check";
+import { useEditListMutation } from "../../../redux/services/listsApiSlice";
+import { showAlert } from "../../../redux/features/alerts/alertsSlice";
+import { useAppDispatch } from "../../../redux/hooks";
+import {
+  startLoading,
+  stopLoading,
+} from "../../../redux/features/modals/modalsSlice";
+
+const SingleListPage = () => {
+  const { listId } = useParams();
+  const dispatch = useAppDispatch();
+  const list = dummyLists.find((list) => Number(listId) === list.id);
+  const [isEditingName, setISEditngName] = useState(false);
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const [editList, { isSuccess, isError }] = useEditListMutation();
+  const [isPrivate, setIsPrivate] = useState(list?.privacy === "PRIVATE");
+
+  const startEditName = () => {
+    setISEditngName(true);
+  };
+  const finishEditName = async () => {
+    dispatch(startLoading());
+    setISEditngName(false);
+    if (!list) return;
+    const title = titleRef.current?.innerText;
+
+    if (!title) {
+      titleRef.current!.innerText = list?.title ?? "Untitled";
+      return;
+    }
+
+    if (title === list.title) return;
+
+    await editList({ id: list.id, listData: { ...list, title } });
+
+    if (isError) {
+      dispatch(
+        showAlert({ message: "Something went wrong", severity: "error" })
+      );
+      titleRef.current.innerText = list.title;
+    }
+
+    if (isSuccess) {
+      dispatch(
+        showAlert({
+          message: "List name updated successfully",
+          severity: "success",
+        })
+      );
+    }
+
+    dispatch(stopLoading());
+  };
+
+  const handleChangePrivacy = async () => {
+    dispatch(startLoading());
+    if (!list) {
+      dispatch(stopLoading());
+      return;
+    }
+
+    const newPrivacy = isPrivate ? "PUBLIC" : "PRIVATE";
+
+    await editList({ id: list.id, listData: { ...list, privacy: newPrivacy } });
+
+    if (isError) {
+      dispatch(
+        showAlert({ message: "Something went wrong", severity: "error" })
+      );
+    }
+
+    if (isSuccess) {
+      dispatch(
+        showAlert({
+          message: `List is now ${newPrivacy.toLowerCase()}`,
+          severity: "success",
+        })
+      );
+      setIsPrivate(!isPrivate);
+    }
+
+    dispatch(stopLoading());
+  };
+
+  titleRef.current?.addEventListener("keydown", async (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      await finishEditName();
+    }
+  });
+
+  if (!list) return <PageNotFoundPage />;
+
+  document.title = `Readify | ${list.title}`;
+  return (
+    <SidePannelLayout>
+      <main className={classes.SingleListPage}>
+        <div className={classes.ListHeader}>
+          <h1
+            className={classes.ListTitle}
+            contentEditable={isEditingName}
+            ref={titleRef}
+            style={{
+              cursor: isEditingName ? "text" : "default",
+              outline: "none",
+              border: isEditingName ? "var(--border)" : "none",
+            }}
+          >
+            {list.title}
+          </h1>
+          <div className={classes.Controllers}>
+            <Button
+              title={isEditingName ? "Save" : "Edit list name"}
+              aria-label="edit"
+              sx={{
+                fontSize: "24px",
+                width: "32px",
+                height: "32px",
+                borderRadius: "50%",
+                minWidth: "36px",
+              }}
+              color="primary"
+              onClick={isEditingName ? finishEditName : startEditName}
+            >
+              {isEditingName ? <CheckIcon /> : <EditIcon />}
+            </Button>
+            <PrivacySwitch
+              isChecked={isPrivate}
+              onChange={handleChangePrivacy}
+            />
+          </div>
+        </div>
+
+        <div className={classes.List}>
+          {list?.books.map((book) => (
+            <div key={book.id}>
+              <BookComponent
+                key={book.id}
+                book={book}
+                currentListId={list.id}
+              />
+            </div>
+          ))}
+        </div>
+      </main>
+    </SidePannelLayout>
+  );
+};
+
+export default SingleListPage;
