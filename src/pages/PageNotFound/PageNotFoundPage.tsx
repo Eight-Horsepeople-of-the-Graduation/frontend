99db456e6b4b9cc3bab7c6a645439d2278b175bf
ranaagouda <ranagouda01@gmail.com>
2024-07-08 09:01:22 +0300

    updates to challenges page css

--- src/pages/PageNotFound/PageNotFoundPage.tsx
+++ src/pages/PageNotFound/PageNotFoundPage.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import { Box } from "@mui/material";
 
 const PageNotFoundPage = () => {
   document.title = "Readify | 404";
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 4, // Adjust unit (px by default) if needed
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh", // Ensures content stays centered even on smaller viewports
+        fontSize: "30px"
+      }}
+    >
       <h1>404</h1>
       <p>Page Not Found</p>
-    </div>
+    </Box>
   );
 };
 
